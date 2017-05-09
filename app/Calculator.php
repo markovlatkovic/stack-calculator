80f8f375df4fed3c9cfb716e4712c422cc1b289f
markovlatkovic <markovlatkovic@outlook.com>
2017-05-09 10:58:10 +0200

    Add docblocks

--- app/Calculator.php
+++ app/Calculator.php
@@ -9,11 +9,21 @@ class Calculator
 {
     private $stack;
 
+    /**
+     * Constructs a stack calculator.
+     *
+     * @param array $stack initial stack, must be an array of numbers
+     */
     public function __construct(array $stack = [])
     {
         $this->stack = new Collection($stack);
     }
 
+    /**
+     * Returns the top of the stack
+     *
+     * @return int stack[top]
+     */
     public function peek()
     {
         return $this->stack->last();
