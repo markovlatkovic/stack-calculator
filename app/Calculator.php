c084f1518bd2d78e929bd34b8e9657fee5eccd8e
markovlatkovic <markovlatkovic@outlook.com>
2017-05-09 11:06:58 +0200

    Implement push()

--- app/Calculator.php
+++ app/Calculator.php
@@ -28,4 +28,19 @@ class Calculator
     {
         return $this->stack->last();
     }
+
+    /**
+     * Pushes a number onto the stack
+     *
+     * @param float $number the number to push onto the stack
+     */
+    public function push(float $number)
+    {
+        $this->stack->push($number);
+    }
+
+    public function getStack()
+    {
+        return $this->stack->all();
+    }
 }
